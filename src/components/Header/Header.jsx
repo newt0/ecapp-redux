45c86f34fbef47e4c9b8275fc7ac05ae1314e48a
newt0 <py_lambda@yahoo.co.jp>
2020-11-10 18:36:39 +0900

    add: zissen/ch9 Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { makeStyles } from "@material-ui/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import logo from "../../assets/img/icons/logo.png";
+import { useSelector } from "react-redux";
+import { getIsSignedIn } from "../../reducks/users/selectors";
+import { push } from "connected-react-router";
+import HeaderMenus from "./HeaderMenus";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuBar: {
+    backgroundColor: "#fff",
+    color: "#444",
+  },
+  toolbar: {
+    margin: "0 auto",
+    maxWidth: 1024,
+    width: "100%",
+  },
+  iconButtons: {
+    margin: "0 0 0 auto",
+  },
+});
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const selector = useSelector((state) => state);
+  const isSignedIn = getIsSignedIn(selector);
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.menuBar}>
+        <Toolbar className={classes.toolbar}>
+          <img
+            src={logo}
+            alt="Logo"
+            width="128px"
+            onClick={() => dispatch(push("/"))}
+          />
+          {isSignedIn && (
+            <div className={classes.iconButtons}>
+              <HeaderMenus />
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/HeaderMenus.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import { Badge } from "@material-ui/core";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import MenuIcon from "@material-ui/icons/Menu";
+import { push } from "connected-react-router";
+
+const HeaderMenus = () => {
+  return (
+    <div>
+      <IconButton>
+        <Badge badgeContent={3} color="secondary">
+          <ShoppingCartIcon />
+        </Badge>
+      </IconButton>
+      <IconButton>
+        <FavoriteBorderIcon />
+      </IconButton>
+      <IconButton>
+        <MenuIcon />
+      </IconButton>
+    </div>
+  );
+};
+
+export default HeaderMenus;
